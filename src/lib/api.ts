8a19638181bdd8509f9f1a1881c597fabcff860b
Alisson Silva <alissonsilva@MacBook-Pro-de-Alisson.local>
2023-05-31 14:46:53 -0300

    novaURL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   //baseURL: "http://localhost:3333",
+  //baseURL: "https://api-correia.onrender.com",
   baseURL: "https://correia-aiq13ss8n-alissonjosef.vercel.app",
 });
