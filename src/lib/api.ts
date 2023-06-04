03d04433451b1f92cc7628948247ba9cbd3854e4
Alisson Silva <alissonsilva@MacBook-Pro-de-Alisson.local>
2023-06-04 09:23:01 -0300

    baseUrl render

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   //baseURL: "http://localhost:3333",
-  //baseURL: "https://api-correia.onrender.com",
-  baseURL: "https://correia-api.vercel.app",
+  baseURL: "https://api-correia.onrender.com",
+  //baseURL: "https://correia-api.vercel.app",
 });
